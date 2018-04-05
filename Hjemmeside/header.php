78faa509130dab5de9a4466e68a0b790388c29ef
Nicklas <sejenick@live.dk>
2018-04-05 13:44:45 +0200

    no message

--- Hjemmeside/header.php
+++ Hjemmeside/header.php
@@ -1,6 +1,22 @@
 <html>
+
 <head>
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
-	<title></title>
+
+	<link rel="stylesheet" type="text/css" href="media/style.css">
+
+	<title>Pikimo-design.dk</title>
+
 </head>
+
 <body>
+
+<h1 id="overskrift">Pikimo</h1>
+
+<ul>
+  <li><a class="active" href="index.php">Hjem</a></li>
+  <li><a href="#">Nyheder</a></li>
+  <li><a href="#">Nyheder</a></li>
+  <li><a href="#">Log in</a></li>
+  <li><a href="#">Opret</a></li>
+  <li><a href="#">Nyheder</a></li>
+</ul>
