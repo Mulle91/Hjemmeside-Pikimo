ccb200ff547a7ba5670e1bd66b20f15430c7199e
Nicklas <sejenick@live.dk>
2018-04-09 13:14:56 +0200

    topnav

--- Hjemmeside/header.php
+++ Hjemmeside/header.php
@@ -12,11 +12,14 @@
 
 <h1 id="overskrift">Pikimo</h1>
 
-<ul>
-  <li><a class="active" href="index.php">Hjem</a></li>
-  <li><a href="#">Nyheder</a></li>
-  <li><a href="#">Nyheder</a></li>
-  <li><a href="#">Log in</a></li>
-  <li><a href="#">Opret</a></li>
-  <li><a href="#">Nyheder</a></li>
+<ul class="topnav">
+  <li><a href="index.php">Hjem</a></li>
+  <li><a href="#1">Nyheder</a></li>
+  <li><a href="#2">Log Ind</a></li>
+  <li><a href="#3">Opret</a></li>
+  <li><a href="#4">Log Ud</a></li>
+  <li><a href="#5">Indkødskurv/Kassen</a></li>
+  <li><a href="#6">Skirft Sprog</a></li>
+  <li><a href="#7">Admin</a></li>
 </ul>
+
